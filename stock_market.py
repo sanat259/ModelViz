2279a629f109af2138fc4f9c26e9e9200bb1d184
Sanat Parmar <sanat259@gmail.com>
2025-01-20 22:59:04 +0530

    streamlit up

--- stock_market.py
+++ stock_market.py
@@ -1,1 +1,8 @@
 import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.title("Stock Market Analysis")
+
+st.sidebar.title("Stock Market Analysis")
+st.write("Simple stats from Market")
